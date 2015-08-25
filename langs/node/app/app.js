aa8190eb990d6e62ba792b3a7ef938a4a8386b25
Ubuntu <ubuntu@ip-172-31-34-97.us-west-2.compute.internal>
2015-08-25 03:18:45 +0000

    Add startServer commands

--- langs/node/app/app.js
+++ langs/node/app/app.js
@@ -4,6 +4,7 @@ var EJS = require('ejs');
 var Utils = require('../../utils.js');
 
 var App = module.exports = {
+  startServer: function(opts) {return 'node server.js'},
   includeView: function(view, options) {
     var code = '';
     if (options.data) {
