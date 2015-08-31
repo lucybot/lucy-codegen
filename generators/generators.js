afb94750075a0dc58cb1e6c35e9796a83d32eefb
Ubuntu <ubuntu@ip-172-31-34-97.us-west-2.compute.internal>
2015-08-31 17:30:18 +0000

    fix generator imports

--- generators/generators.js
+++ generators/generators.js
@@ -1,8 +1,9 @@
+var Path = require('path');
 var FS = require('fs');
 
 var Generators = module.exports = {};
 
 FS.readdirSync(__dirname).forEach(function(f) {
-  if (f === 'generators.js' || FS.statSync(f).isDirectory()) return;
-  Generators[FS.basename(f, '.js')] = require('./' + f);
+  if (f === 'generators.js' || FS.statSync(Path.join(__dirname, f)).isDirectory()) return;
+  Generators[Path.basename(f, '.js')] = require('./' + f);
 })
