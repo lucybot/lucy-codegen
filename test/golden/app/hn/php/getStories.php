46a6919a4db4eed64366001f6489c43315ba0d4f
Ubuntu <ubuntu@ip-172-31-34-97.us-west-2.compute.internal>
2015-09-21 18:47:38 +0000

    update tests

--- test/golden/app/hn/php/getStories.php
+++ test/golden/app/hn/php/getStories.php
@@ -9,20 +9,33 @@ $result = json_decode(curl_exec($curl));
 $result = (object)$result;
 ?>
 <?php foreach($result as $index=>$storyID) { ?>
-  <?php if($index < 10) { ?>
-    <div class="story">
-      <?php echo $storyID ?> 
-      <a class="btn btn-success"
-     data-action="getItem"
-     data-view="itemView"
-     data-answers="{itemID: <?php echo $storyID ?>}">
-     Load
-  </a>
+  <?php if($index == 0) { ?>
+    <div class="item"></div>
+    <script>
+      var element = $('.item').last();
+      element[0].loadData = function() {
+        $('.item').last().load('getItem.php', {
+           itemID: <?php echo json_encode($storyID) ?>,
+        });
+      }
+      element[0].loadData();
+    </script>
 
-    </div>
+  <?php } else { ?>
+    <?php if($index < 10) { ?>
+      <div class="story">
+        <?php echo $storyID ?> 
+        <a class="btn btn-success"
+             data-action="getItem"
+             data-view="itemView"
+             data-answers="{itemID: <?php echo $storyID ?>}">
+             Load
+          </a>
+
+      </div>
+    <?php } ?>
   <?php } ?>
 <?php } ?>
-
 
 <?php
 ?>
